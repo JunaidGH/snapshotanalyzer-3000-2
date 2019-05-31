bfd7b28a683a831b663bfa1337fe5df02d9adf31
Junaid Mohammad <junaid.mohammad@redcloudtechnology.com>
2019-05-31 12:14:19 +0100

    Updated script to know list, snapshots

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -15,12 +15,84 @@ def filter_instances(test):
     return instances
 
 @click.group()
+def cli():
+    """Shotty manages snapshots"""
+
+
+@cli.group('snapshots')
+def snapshots():
+    """ Commands for Snapshots"""
+
+@snapshots.command('list')
+@click.option('--test', default=None,
+    help="Only volumes for project (tag Test:<name>)")
+def list_snapshots(test):
+        "List EC2 Snapshots"
+
+        instances = filter_instances(test)
+
+        for i in instances:
+            for v in i.volumes.all():
+                for s in v.snapshots.all():
+                    print(", ".join((
+                        s.id,
+                        v.id,
+                        i.id,
+                        s.state,
+                        s.progress,
+                        s.start_time.strftime("%c")
+                    )))
+        return
+
+@cli.group('volumes')
+def volumes():
+    """ Commands for Volumes"""
+
+
+@volumes.command('listv')
+@click.option('--test', default=None,
+    help="Only volumes for project (tag Test:<name>)")
+def list_volumes(test):
+        "List the EC2 Volumes"
+
+        instances = filter_instances(test)
+
+        for i in instances:
+            for v in i.volumes.all():
+                print(", ".join((
+                    v.id,
+                    i.id,
+                    v.state,
+                    str(v.size) + "GiB",
+                    v.encrypted and "Encrypted" or "Not Encrypted"
+                )))
+
+        return
+
+@cli.group('instances')
 def instances():
     """Command for instances"""
 
+
+@instance.command('snapshot',
+    help="Create snapshots of all volumes")
+@click.option('--test', default=None,
+    help="Only instances for project (tag Test:<name>)")
+def create_snapshots(test):
+        "Create snapshot for EC2 instances"
+
+        instances = filter_instances(test)
+
+        for i in instances:
+            for v in i.volumes.all():
+                print("Creating snapshot of {0}".format(v.id))
+                v.create_snapshots(Description="Created by SnapShotTest")
+
+        return
+
 @instances.command('list')
 @click.option('--test', default=None,
-    help="Only instances for project (tag ProjectL<name>)")
+    help="Only instances for project (tag Test:<name>)")
 def list_instances(test):
         "List the EC2 Instances"
 
@@ -47,7 +119,6 @@ def running_instances():
         print(instance.id, instance.instance_type)
 
 
-
 @instances.command('start')
 @click.option('--test', default=None,
     help='Only instances for Test prefix')
@@ -61,8 +132,6 @@ def stop_instances(test):
         i.start()
 
     return
-
-
 
 @instances.command('stop')
 @click.option('--test', default=None,
@@ -78,7 +147,6 @@ def stop_instances(test):
 
     return
 
-
-
 if __name__ == "__main__":
-    instances()
+    #instances()
+    cli()
